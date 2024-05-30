d224e0d0691e6c4c65009dc18f830c66dd020f26
ACUVEE <182935@jnu.ac.kr>
2024-05-30 23:59:40 +0900

    correct data structure

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
-dictionanry = {"asd":None, "qwe":1}
+import json
 
-dictionanry["asd"] = {}
-dictionanry["asd"]["zxc"]= 123
-dictionanry["asd"]["xcv"]= 234
 
-print(dictionanry)
+dictionary = {"asd":1, "zxc": 2, "qwe": 2}
+
+print(dictionary)
+
+json_data = json.dumps(dictionary)
+
+print(json_data)
